b0dd931b4f9d2e0692b488047559d00032a7ba87
MAIN\stb6897 <stb6897@rit.edu>
2022-03-27 15:12:35 -0400

    finished domo maker B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,7 +3,10 @@ const models = require('../models');
 const { Account } = models;
 const loginPage = (req, res) => res.render('login');
 const signupPage = (req, res) => res.render('signup');
-const logout = (req, res) => res.redirect('/');
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
 const login = (req, res) => {
   const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
@@ -16,6 +19,8 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong username or password!' });
     }
+
+    req.session.account = Account.toAPI(account);
 
     return res.json({ redirect: '/maker' });
   });
@@ -36,6 +41,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) {
     console.log(err);
